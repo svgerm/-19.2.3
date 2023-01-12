7b9408d3b0b96f697d28ce8f2857d11e21f8589f
svgerm <117482764+svgerm@users.noreply.github.com>
2023-01-12 23:03:05 +0300

    Add files via upload

---
+++ tests/req.py
@@ -0,0 +1,10 @@
+import requests
+
+status = 'available'
+
+res = requests.get(f'https://petstore.swagger.io/v2/pet/findByStatus?status={status}', headers = {'accept':'application/json'})
+
+print(res.status_code)
+print(res.text)
+print(res.json())
+print(type(res.json()))
